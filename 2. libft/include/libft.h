334991b266c6f703a9aaeb6c8077ede616adbdd2
evertonportela <evportel@student.42sp.org.br>
2023-05-01 16:22:10 -0300

    test isalpha isdigit isalnum isascii

--- 2. libft/include/libft.h
+++ 2. libft/include/libft.h
@@ -6,16 +6,17 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 10:02:30 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/01 10:04:24 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:14:38 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdio.h>
-
 /* Mandatory Functions */
-int	ft_isalpha(char *str);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif
