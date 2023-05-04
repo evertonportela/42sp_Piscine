97beebfaf0636ba3d858e3b4cfa52836c1286d5d
evertonportela <evportel@student.42sp.org.br>
2023-05-04 17:43:58 -0300

    update ft_strlcpy and ft_strlcat

--- 2. libft/ft_strlcat.c
+++ 2. libft/ft_strlcat.c
@@ -1,30 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:12:04 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/04 15:58:23 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/04 15:50:21 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	int	end;
 	int	count;
 
-	if (size > 0)
+	if (size == 0)
 	{
-		count = 0;
-		while (count < (size - 1) && src[count] != '\0')
-		{
-			dest[count] = src[count];
-			count++;
-		}
-		dest[count] = '\0';
+		return (ft_strlen(src));
 	}
+	end = ft_strlen(dest);
+	count = 0;
+	while (count < (size) && dest[count] != '\0')
+	{
+		dest[end + count] = src[count];
+		count++;
+	}
+	dest[end + count] = '\0';
 	return (ft_strlen(src));
 }
--- 2. libft/libft.h
+++ 2. libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 10:02:30 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/04 11:07:04 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/04 15:18:21 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
