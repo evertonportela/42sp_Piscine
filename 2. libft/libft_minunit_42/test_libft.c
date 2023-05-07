5b246be5f90c93c32b75ee734524011da8f0f53e
evertonportela <evportel@student.42sp.org.br>
2023-05-07 19:10:31 -0300

    update tests with lib minunit, test ft_memset

--- 2. libft/libft_minunit_42/test_libft.c
+++ 2. libft/libft_minunit_42/test_libft.c
@@ -4,6 +4,8 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 
+
+/* Test - isalpha */
 MU_TEST(isalpha_should_return_true_when_letter_a)
 {
 	int		expected_result = 1;  /* true */
@@ -86,6 +88,7 @@ MU_TEST(isalpha_should_return_false_when_letter_greater_than_Z){
 	mu_assert_int_eq(expected_result, actual_result);
 }
 
+/* Test - isalpha */
 MU_TEST(isdigit_should_return_true_when_letter_0)
 {
 	int		expected_result = 1;  /* true */
@@ -130,6 +133,268 @@ MU_TEST(isdigit_should_return_false_when_letter_greater_than_9)
 	mu_assert_int_eq(expected_result, actual_result);
 }
 
+/* Test - isalnum */
+MU_TEST(isalnum_should_return_false_when_letter_less_than_a)
+{
+	int		expected_result = 0;  /* false */
+	char	tested_char = 'a' - 1;
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_true_when_letter_a)
+{
+	int		expected_result = 1;  /* true */
+	char	tested_char = 'a';
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_false_when_letter_greater_than_z)
+{
+	int		expected_result = 0;  /* false */
+	char	tested_char = 'z' + 1;
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_true_when_letter_z)
+{
+	int		expected_result = 1;  /* true */
+	char	tested_char = 'z';
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_false_when_letter_less_than_A)
+{
+	int		expected_result = 0;  /* false */
+	char	tested_char = 'A' - 1;
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_true_when_letter_A)
+{
+	int		expected_result = 1;  /* true */
+	char	tested_char = 'A';
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_false_when_letter_greater_than_Z)
+{
+	int		expected_result = 0;  /* false */
+	char	tested_char = 'Z' + 1;
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_true_when_letter_Z)
+{
+	int		expected_result = 1;  /* true */
+	char	tested_char = 'Z';
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_true_when_letter_0)
+{
+	int		expected_result = 1;  /* true */
+	char	tested_char = '0';
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_false_when_letter_less_than_0)
+{
+	int		expected_result = 0;  /* false */
+	char	tested_char = '0' - 1;
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_true_when_letter_9)
+{
+	int		expected_result = 1;  /* true */
+	char	tested_char = '9';
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isalnum_should_return_false_when_letter_greater_than_9)
+{
+	int		expected_result = 0;  /* false */
+	char	tested_char = '9' + 1;
+
+	int	actual_result;
+	actual_result = ft_isalnum(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+/* Test - isascii */
+MU_TEST(isascii_should_return_false_when_character_is_less_than_0)
+{
+	int	expected_result = 0; /* false */
+	int	tested_char = -1;
+
+	int actual_result;
+	actual_result = ft_isascii(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isascii_should_return_true_when_character_is_0)
+{
+	int	expected_result = 1; /* true */
+	int	tested_char = 0;
+
+	int actual_result;
+	actual_result = ft_isascii(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isascii_should_return_false_when_character_is_greater_than_127)
+{
+	int	expected_result = 0; /* false */
+	int	tested_char = 128;
+
+	int actual_result;
+	actual_result = ft_isascii(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isascii_should_return_true_when_character_is_127)
+{
+	int	expected_result = 1; /* true */
+	int	tested_char = 127;
+
+	int actual_result;
+	actual_result = ft_isascii(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+/* Test - isprint */
+MU_TEST(isprint_should_return_false_when_preceding_the_space_character)
+{
+	int		expected_result = 0; /* false */
+	char	tested_char = ' ' - 1;
+
+	int actual_result;
+	actual_result = ft_isprint(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isprint_should_return_true_when_is_space_character)
+{
+	int		expected_result = 1; /* true */
+	char	tested_char = ' ';
+
+	int actual_result;
+	actual_result = ft_isprint(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isprint_should_return_false_when_after_the_tilde_character)
+{
+	int		expected_result = 0; /* false */
+	char	tested_char = '~' + 1;
+
+	int actual_result;
+	actual_result = ft_isprint(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(isprint_should_return_true_when_is_tilde_character)
+{
+	int		expected_result = 1; /* true */
+	char	tested_char = '~';
+
+	int actual_result;
+	actual_result = ft_isprint(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+/* Test - strlen */
+MU_TEST(strlen_should_return_3_when_string_have_length_3)
+{
+	int		expected_result = 3; /* true */
+	char	tested_char[] = "123";
+
+	int actual_result;
+	actual_result = ft_strlen(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(strlen_should_return_0_when_string_have_length_0)
+{
+	int		expected_result = 0; /* true */
+	char	tested_char[] = "";
+
+	int actual_result;
+	actual_result = ft_strlen(tested_char);
+
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+/* Test - memset */
+//mu_assert_string_eq
+MU_TEST(memset_should_return_the_pointer_to_position_zero_when_n_is_zero)
+{
+	char	tested_char[100];
+	char	*expected_result = &tested_char[0]; /* true */
+	/* Neste caso, a posição 0 do vetor tested_char é esperada*/
+	
+	char	*actual_result;
+	actual_result = ft_memset(tested_char, 'A', 0);
+	/* ft_memset deve retornar uma posição de memória de tested_char */
+	
+	mu_assert_string_eq(expected_result, actual_result);
+	/* TRUE se a posição de memória recebida em actual_result for igual a posição zero do vetor tested_char */
+}
+
 MU_TEST_SUITE(test_suite)
 {
 	write(1,"test ft_isalpha ", 17);
@@ -149,7 +414,37 @@ MU_TEST_SUITE(test_suite)
 	MU_RUN_TEST(isdigit_should_return_false_when_letter_greater_than_9);
 
 	write(1,"\ntest ft_isalnum ", 18);
-	MU_RUN_TEST();
+	MU_RUN_TEST(isalnum_should_return_false_when_letter_less_than_a);
+	MU_RUN_TEST(isalnum_should_return_true_when_letter_a);
+	MU_RUN_TEST(isalnum_should_return_false_when_letter_greater_than_z);
+	MU_RUN_TEST(isalnum_should_return_true_when_letter_z);
+	MU_RUN_TEST(isalnum_should_return_false_when_letter_less_than_A);
+	MU_RUN_TEST(isalnum_should_return_true_when_letter_A);
+	MU_RUN_TEST(isalnum_should_return_false_when_letter_greater_than_Z);
+	MU_RUN_TEST(isalnum_should_return_true_when_letter_Z);
+	MU_RUN_TEST(isalnum_should_return_true_when_letter_0);
+	MU_RUN_TEST(isalnum_should_return_false_when_letter_less_than_0);
+	MU_RUN_TEST(isalnum_should_return_true_when_letter_9);
+	MU_RUN_TEST(isalnum_should_return_false_when_letter_greater_than_9);
+
+	write(1,"\ntest ft_isascii ", 18);
+	MU_RUN_TEST(isascii_should_return_false_when_character_is_less_than_0);
+	MU_RUN_TEST(isascii_should_return_true_when_character_is_0);
+	MU_RUN_TEST(isascii_should_return_false_when_character_is_greater_than_127);
+	MU_RUN_TEST(isascii_should_return_true_when_character_is_127);
+
+	write(1,"\ntest ft_isprint ", 18);
+	MU_RUN_TEST(isprint_should_return_false_when_preceding_the_space_character);
+	MU_RUN_TEST(isprint_should_return_true_when_is_space_character);
+	MU_RUN_TEST(isprint_should_return_false_when_after_the_tilde_character);
+	MU_RUN_TEST(isprint_should_return_true_when_is_tilde_character);
+
+	write(1,"\ntest ft_strlen ", 17);
+	MU_RUN_TEST(strlen_should_return_3_when_string_have_length_3);
+	MU_RUN_TEST(strlen_should_return_0_when_string_have_length_0);
+
+	write(1,"\ntest ft_memset ", 17);
+	MU_RUN_TEST(memset_should_return_the_pointer_to_position_zero_when_n_is_zero);
 }
 
 int	main(void)
