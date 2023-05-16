feed91d0a797cb1b254b6b3a7b38b5a22f7cca37
evertonportela <evertonportela@outlook.com>
2023-05-16 11:56:22 -0300

    addition - ft_putnbr_fd

--- 2. libft/libft.h
+++ 2. libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 10:02:30 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/15 18:57:31 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/16 10:09:50 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
