26feb7f340b1d3cfbf9f645041c0a3fe51ad3d9c
evertonportela <evertonportela@outlook.com>
2023-05-19 16:55:48 -0300

    addition - ft_lstnew

--- 2. libft/ft_lstnew_bonus.c
+++ 2. libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 18:35:20 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/17 19:25:23 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/19 12:34:05 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	void	content;
+	t_list	*node;
+
+	node = malloc (sizeof(t_list));
+	node->content = content;
+	node->next = NULL;
+	return (node);
 }
